ef62ab61ee9fc4285696ab08721dadcf0ea28bba
mrmojica <mrmojica510@gmail.com>
2017-03-28 10:35:31 -0700

    added custom filter/ min & max

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -2,6 +2,11 @@ angular
 	.module('ngCribs')
 	.controller('cribsController', function($scope, cribsFactory) {
 		$scope.cribs; 
+
+		$scope.priceInfo = {
+			min: 0,
+			max: 1000000
+		}
 		cribsFactory.getCribs().then(function(data) {
 
 			$scope.cribs = data;
@@ -9,9 +14,5 @@ angular
 		}), function(error) {
 			console.log(error);
 		};
-
-		$scope.sayHello = function() {
-			console.log("hello!");
-		}
 
 	});
