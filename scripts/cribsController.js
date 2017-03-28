6433eb93b3afa52fc1277255c299a0b4fa52aad9
mrmojica <mrmojica510@gmail.com>
2017-03-28 11:12:35 -0700

    added edit/delete feature

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -16,6 +16,23 @@ angular
 			$scope.newListing = {};
 		}
 
+		$scope.editCrib = function(crib) {
+			$scope.editListing = true;
+			$scope.existingListing = crib;
+		}
+
+		$scope.saveCribEdit = function() {
+			$scope.existingListing = {};
+			$scope.editListing = false;
+		}
+
+		$scope.deleteCrib = function(listing) {
+			var index = $scope.cribs.indexOf(listing);
+			$scope.cribs.splice(index, 1);
+			$scope.existingListing = {};
+			$scope.editListing = false;
+		}
+
 		cribsFactory.getCribs().then(function(data) {
 
 			$scope.cribs = data;
