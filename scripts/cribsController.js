c7f593cc12ce4099755b699b8cfce08ec2d187a5
mrmojica <mrmojica510@gmail.com>
2017-03-28 09:22:55 -0700

    ng-click/show/model

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -1,6 +1,17 @@
 angular
 	.module('ngCribs')
 	.controller('cribsController', function($scope, cribsFactory) {
-		$scope.cribs = cribsFactory.getCribs();
+		$scope.cribs; 
+		cribsFactory.getCribs().then(function(data) {
+
+			$scope.cribs = data;
+			console.log($scope.cribs)
+		}), function(error) {
+			console.log(error);
+		};
+
+		$scope.sayHello = function() {
+			console.log("hello!");
+		}
 
 	});
